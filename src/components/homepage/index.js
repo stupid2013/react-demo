7d49734bce43bb04ecd6c1b8e0a626fee8e45c16
stupid <stupid.du@coloseo.cn>
2018-05-08 17:13:14 +0800

    update

---
+++ src/components/homepage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { hashHistory } from 'react-router';
+import styles from './index.css';
+
+export default () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1><a onClick={() => hashHistory.push('/canvas')}>基于konvas和react-canvas的涂鸦测试(demo of konvas)</a></h1>
+    </div>
+  );
+};
